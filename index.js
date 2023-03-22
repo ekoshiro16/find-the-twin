e667e28de7f558d571fcf1a04944fb5170854512
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-21 19:26:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+You are a caretaker at an elderly persons' home. What's unusual about your care home, though, is that you have an unusual number of twins residing here! And to make matters worse, the elderly twins often tend to get lost in the building, and have to be retrieved by the care home staff. With you knowledge of web development, you want to help out the home. 
+
+Write an algorithm that accepts a string as an input, where each individual character in the string represents a resident of the care home. 
+
+Your algorithm should return the first character that repeats in the string.
+
+However, if no repeating character exists in the input string, your algorithm should return undefined. 
+
+For example: 
+If you were given the following string: "abcdec"
+Note that the "c" character is the first to repeat, so your algorithm returns "c". 
+
+If you were given the following string: "abcde"
+There are no repeating characters, so it returns undefined.
+
+If you were given the following string: "abcca"
+Your algorithm would return "c". 
+Why? Note that even though the letter "a" does eventually repeat in the string, the letter "c" is the first to actually begin repeating. 
+
+Note: You may not use the built-in .find method in this algorithm. 
+*/
+
+// Write your code below: 
